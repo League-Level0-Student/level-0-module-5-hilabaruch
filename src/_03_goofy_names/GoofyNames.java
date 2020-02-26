527d2ecb8f27d536eac9eac1a76921f9ccfa002d
League Student <league@iMac-14.attlocal.net>
2020-02-25 19:57:39 -0800

    hi :smiley:

--- src/_03_goofy_names/GoofyNames.java
+++ src/_03_goofy_names/GoofyNames.java
@@ -11,14 +11,23 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+String a = JOptionPane.showInputDialog(null,"What's your name?");
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
-
+ System.out.println(a.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 6).
 		//    HINT: Use .length() to determine the number of characters in the String.
-
-			// 4. Create a String variable to store the next character of the name
+for (int i = 0; i < a.length(); i++) {
+	   String b = a.substring(i, i+1);
+if(i%2==0) {
+System.out.println(b.toUpperCase());
+b=b.toUpperCase();
+}
+else{
+	System.out.println(b.toLowerCase());
+	b=b.toLowerCase();
+}
+// 4. Create a String variable to store the next character of the name
 			//    using .substring(start, end)
 			//    HINT: replace 'start' and 'end' to get String with character at i 
 			
@@ -27,10 +36,10 @@ public class GoofyNames {
 
 			
 			// 6. ADD this String  (containing 1 char) to the goofyName String
-
-		
+goofyName = goofyName+b;
+}
 		// 7. Use pop-up to show user their Goofy name
-
+JOptionPane.showMessageDialog(null, goofyName);
 	}
 }
 
